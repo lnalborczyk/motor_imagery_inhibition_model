f9a3a5b61a11bc7fb60317ca7864177196704756
Ladislas Nalborczyk <lnalborczyk@users.noreply.github.com>
2023-03-20 15:50:15 +0100

    speeding up the optimisation algorithm

--- fitting.R
+++ fitting.R
@@ -4,7 +4,7 @@
 # ----------------------------------------- #
 # Written by Ladislas Nalborczyk            #
 # E-mail: ladislas.nalborczyk@gmail.com     #
-# Last updated on March 9, 2023             #
+# Last updated on March 20, 2023            #
 #############################################
 
 library(DEoptim) # global optimisation by differential evolution
@@ -14,19 +14,21 @@ library(pso) # particle swarm optimisation
 
 # simulating some data and computing the prediction error
 loss_function <- function (
-        par = c(1, 1, 1), data,
+        par = c(1, 1), data,
         nsims = 100, nsamples = 2000,
         exec_threshold = 1, imag_threshold = 0.5, iti = 2
         ) {
     
     # retrieving parameter values
-    amplitude_activ <- par[[1]]
+    amplitude_activ <- 1.5
     peak_time_activ <- 0.5
     curvature_activ <- 0.4
-    amplitude_inhib <- par[[2]]
-    peak_time_inhib <- 0.5
+    # amplitude_inhib is expressed in % of amplitude_activ
+    # peak_time_inhib is expressed in % of peak_time_activ
+    amplitude_inhib <- par[[1]] * amplitude_activ
+    peak_time_inhib <- par[[2]] * peak_time_activ
     curvature_inhib <- 0.4
-    amplitude_inhib_prev <- par[[3]]
+    amplitude_inhib_prev <- 0.5
     peak_time_inhib_prev <- 0.5
     curvature_inhib_prev <- 0.6
     
@@ -49,26 +51,26 @@ loss_function <- function (
     # amplitude_inhib should be >= amplitude_activ in imagined trials...
     # amplitude_activ should be >= amplitude_inhib in executed trials...
     if (unique(data$action_mode) == "imagined" & amplitude_activ > amplitude_inhib) {
-        
+
         prediction_error <- 1e6
         return (prediction_error)
-        
+
     } else if (unique(data$action_mode) == "executed" & amplitude_inhib > amplitude_activ) {
-        
+
         prediction_error <- 1e6
         return (prediction_error)
-        
+
     }
     
     # retrieving distribution of simulated RTs
     if (unique(data$action_mode) == "imagined") {
         
         predicted_rt <- results %>%
-        dplyr::select(sim, rt = onset_imag) %>%
-        distinct() %>%
-        pull(rt) %>%
-        # replacing NAs with a very large value
-        replace_na(1e6)
+            dplyr::select(sim, rt = onset_imag) %>%
+            distinct() %>%
+            pull(rt) %>%
+            # replacing NAs with a very large value
+            replace_na(1e6)
         
         # retrieving distribution of simulated MTs
         predicted_mt <- results %>%
@@ -79,7 +81,7 @@ loss_function <- function (
             replace_na(1e6)
         
     } else if (unique(data$action_mode) == "executed") {
-    
+        
         predicted_rt <- results %>%
             dplyr::select(sim, rt = onset_exec) %>%
             distinct() %>%
@@ -101,44 +103,50 @@ loss_function <- function (
         
     }
     
-    # quantile-based loss function (e.g., Ratcliff & Smith, 2004)
+    # computes the proportion of observations within quantiles
     find_quantiles_props <- function(x, quants) {
         
         quants2 <- c(0, quants, Inf) %>% as.numeric()
-        quants_props <- numeric(length = length(quants) + 1)
         
-        for (i in 1:length(quants_props) ) {
-            
-            quants_props[i] <- mean(quants2[i] < x & x <= quants2[i + 1])
-            
-        }
+        # first option
+        # quants_props <- numeric(length = length(quants) + 1)
+        
+        # for (i in 1:length(quants_props) ) {
+        #     
+        #     quants_props[i] <- mean(quants2[i] < x & x <= quants2[i + 1])
+        #     # quants_props[i] <- mean(quants2[i] <= x & x < quants2[i + 1])
+        #     
+        # }
+        
+        # second option (probably faster)
+        quants_props <- as.numeric(table(cut(x, quants2) ) ) / length(x)
         
         return (quants_props)
         
     }
-
-    # computing observed RT quantiles
-    observed_rt_quantiles <- quantile(x = data$reaction_time, probs = c(0.1, 0.3, 0.5, 0.7, 0.9) )
     
-    # computing observed MT quantiles
-    observed_mt_quantiles <- quantile(x = data$movement_time, probs = c(0.1, 0.3, 0.5, 0.7, 0.9) )
+    # computes observed RT quantiles
+    observed_rt_quantiles <- quantile(x = data$reaction_time, probs = c(0.1, 0.3, 0.5, 0.7, 0.9), na.rm = TRUE)
     
-    # computing observed proportion of data in RT quantiles
+    # computes observed MT quantiles
+    observed_mt_quantiles <- quantile(x = data$movement_time, probs = c(0.1, 0.3, 0.5, 0.7, 0.9), na.rm = TRUE)
+    
+    # computes observed proportion of data in RT quantiles
     observed_rt_quantiles_props <- find_quantiles_props(x = data$reaction_time, quants = observed_rt_quantiles)
     
-    # computing observed proportion of data in MT quantiles
+    # computes observed proportion of data in MT quantiles
     observed_mt_quantiles_props <- find_quantiles_props(x = data$movement_time, quants = observed_mt_quantiles)
     
-    # computing predicted proportion of data in RT quantiles
+    # computes predicted proportion of data in RT quantiles
     predicted_rt_quantiles_props <- find_quantiles_props(x = predicted_rt, quants = observed_rt_quantiles)
     
-    # computing predicted proportion of data in MT quantiles
+    # computes predicted proportion of data in MT quantiles
     predicted_mt_quantiles_props <- find_quantiles_props(x = predicted_mt, quants = observed_mt_quantiles)
     
     # applies a small correction when prop = 0 to avoid negative or Inf g-square
     predicted_rt_quantiles_props <- ifelse(
         test = predicted_rt_quantiles_props == 0,
-        yes = predicted_rt_quantiles_props + 0.001,
+        yes = 0.0001,
         no = predicted_rt_quantiles_props
         )
     
@@ -148,7 +156,7 @@ loss_function <- function (
     # applies a small correction when prop = 0 to avoid negative or Inf g-square
     predicted_mt_quantiles_props <- ifelse(
         test = predicted_mt_quantiles_props == 0,
-        yes = predicted_mt_quantiles_props + 0.001,
+        yes = 0.0001,
         no = predicted_mt_quantiles_props
         )
     
@@ -157,18 +165,16 @@ loss_function <- function (
     
     # computes the G^2 prediction error (combined for RTs and MTs)
     # see Ratcliff & Smith (2004, doi:10.1037/0033-295X.111.2.333) or Servant et al. (2019, doi:10.1152/jn.00507.2018.)
-    # computes the prediction error for both executed and imagined RTs/MTs?
-    # this would constrain the activation/inhibition amplitude
     prediction_error <- 2 * (
         # error for RTs
         sum(observed_rt_quantiles_props * log(observed_rt_quantiles_props / predicted_rt_quantiles_props) ) +
             # error for MTs
             sum(observed_mt_quantiles_props * log(observed_mt_quantiles_props / predicted_mt_quantiles_props) )
-        )
+    )
     
     # returns the prediction error
     return (prediction_error)
-    
+
 }
 
 # fitting the model
@@ -218,9 +224,19 @@ model_fitting <- function (
             
             fit <- DEoptim::DEoptim(
                 fn = loss_function,
-                lower = c(0, 0, 0), #, 0, 0, 0),
-                upper = c(3, 3, 3), #, 3, 3, 3),
-                control = DEoptim.control(itermax = maxit, trace = 2),
+                lower = c(0, 0), #, 0), #, 0, 0, 0),
+                upper = c(2, 2), #, 3), #, 3, 3, 3),
+                control = DEoptim.control(
+                    itermax = maxit, trace = 2,
+                    # defines the differential evolution strategy (defaults to 2)
+                    # strategy = 6,
+                    # c controls the speed of the crossover adaptation (defaults to 0)
+                    # VTR = 0, c = 0.5,
+                    # using all available cores
+                    parallelType = "parallel",
+                    packages = c("DEoptim", "tidyverse"),
+                    parVar = c("model", "loss_function")
+                    ),
                 data = df
                 )
             
--- parameter_recovery.R
+++ parameter_recovery.R
@@ -3,7 +3,7 @@
 # ------------------------------------------ #
 # Written by Ladislas Nalborczyk             #
 # E-mail: ladislas.nalborczyk@gmail.com      #
-# Last updated on March 9, 2023              #
+# Last updated on March 20, 2023             #
 ##############################################
 
 # importing the data-generating model
@@ -12,24 +12,45 @@ source(file = "model.R")
 # importing the model fitting routines
 source(file = "fitting.R")
 
-# simulating some data in EI sequences
+# true parameter values in EI sequences, which refers to the proportion of
+# activation amplitude for inhibition and inhibition_previous amplitudes, respectively
+true_pars <- c(1, 0.75)
+
+# simulating data
 df <- model(
     nsims = 100, nsamples = 2000,
     exec_threshold = 1, imag_threshold = 0.5, iti = 2,
-    amplitude_activ = 1.5, peak_time_activ = 0.5, curvature_activ = 0.4,
-    amplitude_inhib = 1.5, peak_time_inhib = 0.5, curvature_inhib = 0.6,
-    amplitude_inhib_prev = 0.5, peak_time_inhib_prev = 0.5, curvature_inhib_prev = 0.6
+    amplitude_activ = 1.5,
+    peak_time_activ = 0.5,
+    curvature_activ = 0.4,
+    amplitude_inhib = true_pars[1] * 1.5,
+    peak_time_inhib = true_pars[2] * 0.5,
+    curvature_inhib = 0.6,
+    amplitude_inhib_prev = 0.5,
+    peak_time_inhib_prev = 0.5,
+    curvature_inhib_prev = 0.6
     ) %>%
-    # keeping only imagery RTs and MTs (for testing purposes)
-    dplyr::select(sim, reaction_time = onset_imag, movement_time = mt_imag) %>%
+    # was the action executed or imagined?
+    mutate(action_mode = ifelse(test = true_pars[1] >= 1, yes = "imagined", no = "executed") ) %>%
+    # keeping only the relevant columns
+    dplyr::select(
+        sim,
+        reaction_time = paste0("onset_", substr(unique(.$action_mode), 1, 4) ),
+        movement_time = paste0("mt_", substr(unique(.$action_mode), 1, 4) ),
+        action_mode
+        ) %>%
     distinct() %>%
-    dplyr::select(-sim) %>%
-    # specifying the current action mode (i.e., executed or imagined)
-    mutate(action_mode = "imagined")
+    dplyr::select(-sim)
 
 # plotting the distributions of RTs and MTs
-hist(x = df$reaction_time, breaks = "FD")
-hist(x = df$movement_time, breaks = "FD")
+df %>%
+    pivot_longer(cols = reaction_time:movement_time) %>%
+    ggplot(aes(x = value, colour = name, fill = name) ) +
+    geom_density(alpha = 0.5, show.legend = FALSE) +
+    theme_bw(base_size = 12, base_family = "Open Sans") +
+    scale_fill_manual(values =  met.brewer(name = "Johnson", n = 2) ) +
+    scale_colour_manual(values = met.brewer(name = "Johnson", n = 2) ) +
+    labs(x = "Reaction/Movement time (in seconds)", y = "Density")
 
 # fitting the model using all methods available in optimx::optimx()
 # fitting_results <- model_fitting(data = df, method = "all_methods", maxit = 1e2)
@@ -44,6 +65,12 @@ hist(x = df$movement_time, breaks = "FD")
 # fitting_results$value
 # fitting_results$par
 
-# fitting the model using differential evolution (seems to work best with 3+ pars)
-fitting_results <- model_fitting(data = df, method = "DEoptim", maxit = 1e2)
-fitting_results$optim
+# fitting the model using differential evolution (seems to work best)
+fitting_results <- model_fitting(data = df, method = "DEoptim", maxit = 100)
+
+# plotting the optimisation results
+# plot(x = fitting_results, plot.type = "bestmemit", type = "b", col = "steelblue")
+# plot(x = fitting_results, plot.type = "bestvalit", type = "b", col = "steelblue")
+
+# getting a summary of the optimisation results
+summary(fitting_results)
